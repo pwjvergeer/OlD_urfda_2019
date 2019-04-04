df941c5468ef8bb86f495dab238f03e3de6c6590
Marc Teunis <marc.teunis@hu.nl>
2019-04-04 16:32:21 +0200

    new structure, gganimate, demo, render.R

--- code/r_packages_graph.R
+++ code/r_packages_graph.R
@@ -27,17 +27,32 @@ page %>%
   summarise(published = min(count)) %>%
   mutate(Date = as.Date(as.yearmon(Month))) -> pkgs
 
-## generate graphs
-p <- ggplot(data = pkgs, aes(x = Date, y = published)) 
-plot <- p + geom_point() + geom_line() + theme_bw() +
-  labs(title = "CRAN packages published from 2005 - yesterday") +
-  xlab("Date") 
-plot  
+## pkgs_summary
+pkgs_summary <- pkgs %>%
+  mutate(
+  Year = lubridate::year(Date)
+  ) %>%
+  arrange(Year) %>%
+  group_by(Year) %>%
+  summarize(total_pkgs_year = sum(published)) %>%
+  dplyr::filter(Year != "2019")
 
-ggsave("./images/cran_pkgs_published.png", plot = plot)
-margins = list(l = 100, r = 100, b = 100, t = 100, pad = 4)
-pkgs %>%
-  plot_ly(x=Date, y=published, name="Published packages") %>%
-  layout(title = "CRAN packages published ever since.", margin = margins)
+## plot
+plot <- pkgs_summary %>%
+  ggplot(aes(x = Year, y = total_pkgs_year)) + 
+  geom_point(size = 2.5, colour = "darkblue") + 
+  geom_line(colour = "steelblue", size = 1.5) + 
+  theme_bw() +
+  labs(title = paste("CRAN packages published from",
+                     min(pkgs_summary$Year), "to", Sys.Date())) +
+  xlab("Year") +
+  ylab("Total packages/year") +
+  scale_x_continuous(breaks=seq(2006, 2018, 2)) +
+  scale_y_continuous(labels = function(x) format(x, scientific = TRUE))
+  
+# plot
 
+#ggsave("./images/cran_pkgs_published.png", plot = plot)
+#margins = list(l = 100, r = 100, b = 100, t = 100, pad = 4)
 
+plot
